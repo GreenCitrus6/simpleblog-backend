98b138b23a0792d40699667e145eef4bb3510f6d
GreenCitrus6 <DanielAAad01@gmail.com>
2026-06-24 14:36:15 +0900

    connected allPosts() endpoint with MongoDB

--- src/main/java/me/aadland/simpleblog/Post.java
+++ src/main/java/me/aadland/simpleblog/Post.java
@@ -10,7 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document
+@Document(collection="posts")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,4 +22,41 @@ public class Post {
 	private String content;
 	private String category;
 	private List<String> tags;
+	
+	//getters
+	public ObjectId getObjectId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public List<String> getTags() {
+		return tags;
+	}
+	
+	//setters
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+	
+	//constructors
+	
 }
